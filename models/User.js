aa4f8dbebdb7e6b479598c496196437d1fb40d33
jyoti patel <jyotipatelfyit77100@gmail.com>
2022-04-03 11:20:03 +0530

    server setup

---
+++ models/User.js
@@ -0,0 +1,42 @@
+import mongoos from 'mongoose'
+import validetar from 'validator'
+const UserSchema = new mongoos.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide name'],
+    minlength: 3,
+    maxlength: 20,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide password'],
+    minlength: 6,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide email'],
+    validate: {
+      validator: validetar.isEmail,
+      message: 'Please provide valid email',
+    },
+    unique: true,
+  },
+  photo: {
+    type: String,
+    default: null,
+  },
+  phone: {
+    type: Number,
+    default: null,
+    maxlength: 10,
+  },
+  bio: {
+    type: String,
+    default: 'Hi, there!',
+    maxlength: 100,
+    trim: true,
+  },
+})
+
+export default mongoos.model('User', UserSchema)
